c28f74141e512f11b3b92fe243f32764647efc96
Enzo Poggio <poggio.enzo@gmail.com>
2023-11-11 13:06:41 -0500

    black that

--- python/graph.py
+++ python/graph.py
@@ -8,7 +8,7 @@ from collections import Counter
 HERE = pathlib.Path(__file__).parent
 
 
-PAGES_DIR = HERE / "content"/"pages"
+PAGES_DIR = HERE / "content" / "pages"
 themes_counter = Counter()
 
 shortcode_pattern = r'\{\{< themes theme="([^"]+)" >\}\}'
@@ -16,7 +16,7 @@ shortcode_pattern = r'\{\{< themes theme="([^"]+)" >\}\}'
 for root, dirs, files in os.walk(PAGES_DIR):
     for file in files:
         if file.endswith(".md"):
-            with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+            with open(os.path.join(root, file), "r", encoding="utf-8") as f:
                 post = frontmatter.load(f)
                 content = post.content
                 shortcodes = re.findall(shortcode_pattern, content)
@@ -28,16 +28,17 @@ for root, dirs, files in os.walk(PAGES_DIR):
 
 
 # Conversion du compteur en DataFrame pour la création du graphique
-themes_df = pd.DataFrame.from_dict(themes_counter, orient='index', columns=['Occurrence'])
-themes_df = themes_df.sort_values(by='Occurrence', ascending=False)
+themes_df = pd.DataFrame.from_dict(
+    themes_counter, orient="index", columns=["Occurrence"]
+)
+themes_df = themes_df.sort_values(by="Occurrence", ascending=False)
 
 # Création d'un diagramme à barres
-plt.bar(themes_df.index, themes_df['Occurrence'])
-plt.xlabel('Thématique')
-plt.ylabel('Occurrences')
-plt.xticks(rotation=45, ha='right')
+plt.bar(themes_df.index, themes_df["Occurrence"])
+plt.xlabel("Thématique")
+plt.ylabel("Occurrences")
+plt.xticks(rotation=45, ha="right")
 plt.tight_layout()
 
 # Affichage ou sauvegarde du graphique
 plt.show()
-
